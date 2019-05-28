60a8b8200fb23faf96535e6766d4b7c34df34c40
Josh Perry <josh.perry@roostermornings.com>
2019-05-28 15:59:00 -0500

    Added index skeleton

---
+++ index.js
@@ -0,0 +1,20 @@
+/**
+ * Function entry point
+ * Lambda function to implement a client API
+ * 
+ * Collaborator(s): Josh Perry <josh.perry245@gmail.com>
+ * Created: 04/18/2019
+ */
+
+const axios     = require('axios');
+const Constants = require('./Config/Constants');
+
+exports.handler = async (event) => {
+    try {
+
+    } 
+    catch(error) {
+
+    } 
+}
+
